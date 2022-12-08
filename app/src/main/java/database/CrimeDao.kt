d2185318efe1ae4c7cf138df2757ece913c27f4b
Gabriel Salas <gabriel.salas3321@alumnos.udg.mx>
2022-12-08 03:11:52 -0600

    Programa terminado

---
+++ app/src/main/java/database/CrimeDao.kt
@@ -0,0 +1,28 @@
+package database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import salas.gabriel.criminalintent.Crime
+import java.util.UUID
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CrimeDao{
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): Flow<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id = (:id)")
+    suspend fun getCrime(id: UUID): Crime
+
+    @Update
+    suspend fun updateCrime(crime: Crime)
+
+    @Insert
+    suspend fun addCrime(crime: Crime)
+
+    @Delete
+    suspend fun removeCrime(crime: Crime)
+}
